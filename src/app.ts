0577f9d025a65847335058a5775aea7fba990018
Mikel Galdos <mikelgaldosv@gmail.com>
2021-10-02 05:25:50 +0200

    socket.io

--- src/app.ts
+++ src/app.ts
@@ -1,15 +1,22 @@
 import express from "express";
+import http from "http";
 import bodyparser from "body-parser";
+import { Server } from "socket.io";
 
 const app = express();
+const server = http.createServer(app)
 const PORT = '3000';
+const io = new Server(server);
+
 
 app.get('/', (req, res) => {
     res.send('<h1>Hello</h1>')
 });
 
+io.on('connection', (socket) => {
+    console.log('a user connected');
+});
 
-
-app.listen(PORT, () => {
+server.listen(PORT, () => {
     console.log('Listenint on *:3000');
 });
