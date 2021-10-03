a3cbd65a9a19ad675f63f44be26cd0f87271a604
Mikel Galdos <mikelgaldosv@gmail.com>
2021-10-03 17:50:11 +0200

    thing

--- src/routes/auth.ts
+++ src/routes/auth.ts
@@ -9,15 +9,24 @@ import * as authController from "../controllers/auth";
 const router = Router();
 
 router.post('/signup', [
-    body('email').isEmail().withMessage('Please enter a valid email.').custom((value, {req}) => {
-        return User.findOne({email:value}).then(userDoc => {
-            if (userDoc) {
-                return Promise.reject('E-mail addres already exists!')
-            }
-        });
-    }).normalizeEmail(),
-    body('password').trim().isLength({ min: 5 }),
-    body('name').trim().not().isEmpty()
+    body('email')
+        .isEmail()
+        .withMessage('Please enter a valid email.')
+        .custom((value, {req}) => {
+            return User.findOne({email:value}).then(userDoc => {
+                if (userDoc) {
+                    return Promise.reject('E-mail addres already exists!')
+                }
+            });
+        })
+        .normalizeEmail(),
+    body('name')
+        .trim()
+        .not()
+        .isEmpty(),
+    body('password')
+        .trim()
+        .isLength({ min: 5 })
 ],authController.signup
 );
 
