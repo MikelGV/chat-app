593dfc76eaff34487fb49e0e7a2b6d68485fbd1f
Mikel Galdos <mikelgaldosv@gmail.com>
2021-10-04 02:39:03 +0200

    feed routes imports added

---
+++ src/routes/feed.ts
@@ -0,0 +1,9 @@
+import { Router } from "express";
+import { body } from "express-validator/check";
+
+import * as feedController from "../controllers/feed";
+
+const router = Router();
+
+
+export { router };
