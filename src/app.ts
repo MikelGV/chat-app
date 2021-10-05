f28da197984d2eb03ec205d6d27c09596e0ea330
Mikel Galdos <mikelgaldosv@gmail.com>
2021-10-06 00:40:25 +0200

    things moved from app to server.ts

--- src/app.ts
+++ src/app.ts
@@ -1,38 +1,26 @@
 // Third party imports
 import express from "express";
-import { createServer } from "http";
-import { Server } from "socket.io";
-import cors from "cors";
-import config from "config";
-import mongoose from "mongoose";
+import { Server, Socket } from "socket.io";
 
 // My imports
-import logger from "./utils/logger"
-import { DB_PASS } from "./secret";
-import socket from "./socket";
-
-const port = config.get<number>("port");
-const host = config.get<string>("host");
-const corsOrigin = config.get<string>("corsOrigin")
+import logger from "./utils/logger";
 
 const app = express();
 
-const httpServer = createServer(app)
-
-const io = new Server(httpServer, {
-    cors: {
-        origin: corsOrigin,
-        credentials: true,
-    },
-});
-
+// routes
 app.get('/', (_, res) => res.send("Server is up"))
 
-mongoose.connect(DB_PASS).then(resutl => {
-    httpServer.listen(port, host, () => {
-        logger.info("Server is listening");
-        logger.info(`http::/${host}:${port}`);
+// socket.io
+const EVENTS = {
+    connection: 'connection'
+}
 
-        socket({io});
-    });
-});
+function socket({io}:{io: Server}){
+    logger.info("Sockets enabled");
+
+    io.on(EVENTS.connection, (socket: Socket) => {
+        logger.info(`User connected ${socket.id}`);
+    })
+};
+
+export default socket
