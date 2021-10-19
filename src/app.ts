e70e5c3b181524168ee9e788cce253f3dcf35c36
Mikel Galdos <mikelgaldosv@gmail.com>
2021-10-19 06:18:03 +0200

    events changed

--- src/app.ts
+++ src/app.ts
@@ -13,15 +13,18 @@ app.get('/', (_, res) => res.send("Server is up"))
 
 // socket.io
 const EVENTS = {
-    connection: 'connection',
+    connection: "connection",
     CLIENT: {
-        CREATE_ROOM: "CREATE_ROOM"
+        CREATE_ROOM: "CREATE_ROOM",
+        SEND_ROOM_MESSAGE: "SEND_ROOM_MESSAGE",
+        JOIN_ROOM: "JOIN_ROOM",
     },
     SERVER: {
         ROOMS: "ROOMS",
-        JOINED_ROOM: "JOINED_ROOM"
-    }
-}
+        JOINED_ROOM: "JOINED_ROOM",
+        ROOM_MESSAGE: "ROOM_MESSAGE",
+    },
+};
 
 const rooms: Record<string, {name: string}> = {};
 
@@ -44,8 +47,20 @@ function socket({io}:{io: Server}){
             socket.broadcast.emit(EVENTS.SERVER.ROOMS, rooms);
 
             socket.emit(EVENTS.SERVER.JOINED_ROOM, roomId);
+        });
+
+        socket.on(EVENTS.CLIENT.SEND_ROOM_MESSAGE, ({roomId, message, username}) => {
+            const date = new Date();
+
+            socket.to(roomId).emit(EVENTS.SERVER.ROOM_MESSAGE, {
+                message,
+                username,
+                time: `${date.getHours()}:${date.getMinutes()}`,
+            });
         })
-    })
+    });
+
+
 };
 
 export default socket
