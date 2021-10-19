267d61f60084047f1173280a75fc815141e43021
Mikel Galdos <mikelgaldosv@gmail.com>
2021-10-19 04:06:25 +0200

    server events added

--- src/app.ts
+++ src/app.ts
@@ -1,6 +1,7 @@
 // Third party imports
 import express from "express";
 import { Server, Socket } from "socket.io";
+import { nanoid } from "nanoid";
 
 // My imports
 import logger from "./utils/logger";
@@ -15,8 +16,14 @@ const EVENTS = {
     connection: 'connection',
     CLIENT: {
         CREATE_ROOM: "CREATE_ROOM"
+    },
+    SERVER: {
+        ROOMS: "ROOMS",
+        JOINED_ROOM: "JOINED_ROOM"
     }
 }
+
+const rooms: Record<string, {name: string}> = {};
 
 function socket({io}:{io: Server}){
     logger.info("Sockets enabled");
@@ -26,6 +33,17 @@ function socket({io}:{io: Server}){
 
         socket.on(EVENTS.CLIENT.CREATE_ROOM, ({roomName}) => {
             console.log({roomName})
+
+            const roomId = nanoid();
+
+            rooms[roomId] = {
+                name: roomName,
+            };
+            socket.join(roomId);
+
+            socket.broadcast.emit(EVENTS.SERVER.ROOMS, rooms);
+
+            socket.emit(EVENTS.SERVER.JOINED_ROOM, roomId);
         })
     })
 };
